46c7c4bab04c8f8e9b75855bddcede0739203d9f
Albar Adimas Suntoro <albar230505@gmail.com>
2023-08-01 10:29:58 +0700

    feat: able to get by id, update and delete uom

--- delivery/controller/api/uom_controller.go
+++ delivery/controller/api/uom_controller.go
@@ -23,7 +23,14 @@ func (u *UomController) createHandler(c *gin.Context) {
 		return
 	}
 
-	c.JSON(201, uom)
+	status := map[string]any{
+		"code":        200,
+		"description": "Create Data Successfully",
+	}
+	c.JSON(201, gin.H{
+		"status": status,
+		"data":   uom,
+	})
 
 }
 func (u *UomController) listHandler(c *gin.Context) {
@@ -32,17 +39,71 @@ func (u *UomController) listHandler(c *gin.Context) {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(200, uoms)
+	status := map[string]any{
+		"code":        200,
+		"description": "Get All Data Successfully",
+	}
+	c.JSON(200, gin.H{
+		"status": status,
+		"data":   uoms,
+	})
 }
 func (u *UomController) getHandler(c *gin.Context) {
+	id := c.Param("id")
+	uom, err := u.uomUC.FindByIdUom(id)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
 
+	status := map[string]any{
+		"code":        200,
+		"description": "Get By ID Data Successfully",
+	}
+	c.JSON(200, gin.H{
+		"status": status,
+		"data":   uom,
+	})
 }
 
 func (u *UomController) updateHandler(c *gin.Context) {
+	var uom model.Uom
+	if err := c.ShouldBindJSON(&uom); err != nil {
+		c.JSON(400, gin.H{"err": err.Error()})
+		return
+	}
 
+	if err := u.uomUC.UpdateUom(uom); err != nil {
+		c.JSON(500, gin.H{"err": err.Error()})
+		return
+	}
+
+	status := map[string]any{
+		"code":        200,
+		"description": "Update Data Successfully",
+	}
+	c.JSON(200, gin.H{
+		"status": status,
+		"data":   uom,
+	})
 }
 
 func (u *UomController) deleteHandler(c *gin.Context) {
+	id := c.Param("id")
+	err := u.uomUC.DeleteUom(id)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	status := map[string]any{
+		"code":        200,
+		"description": "Delete Data Successfully",
+	}
+	c.JSON(200, gin.H{
+		"status": status,
+	})
 
 }
 
@@ -56,7 +117,7 @@ func NewUomController(usecase usecase.UomUseCase, r *gin.Engine) *UomController 
 	r.GET("/uoms", controller.listHandler)
 	r.GET("/uoms/:id", controller.getHandler)
 	r.PUT("/uoms", controller.updateHandler)
-	r.DELETE("/uoms", controller.deleteHandler)
+	r.DELETE("/uoms/:id", controller.deleteHandler)
 
 	return &controller
 }
